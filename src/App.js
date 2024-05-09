7da933f0963b749889bb07da809f6385f4210b95
SocoliucRobert <ionut.socoliuc@student.usv.ro>
2024-05-09 17:41:53 +0300

    Remove bloatware, instalare module pt styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import styles from './redbox.module.css';
+import blueStyles from './bluebox.module.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    
+      <div className={styles["red-box"]}></div>
+      <div className={blueStyles["red-box"]}></div>
+       
+     
     </div>
   );
 }
